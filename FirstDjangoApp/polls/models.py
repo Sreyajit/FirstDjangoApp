3e18b0c2231e75b95bbe4dfc232dd5d2f7199be1
Sreyajit <sreyajit25@gmail.com>
2022-12-22 16:20:37 +0530

    Activating Models after referencing app

--- FirstDjangoApp/polls/models.py
+++ FirstDjangoApp/polls/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField(default=timezone.now)
+    pub_date = models.DateTimeField(auto_now = True)
 class Choice(models.Model):
     question= models.ForeignKey(Question,  on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=240)
