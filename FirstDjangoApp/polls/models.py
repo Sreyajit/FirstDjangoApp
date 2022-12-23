7cc0b062ed2e42bfa64f70ed7a1a73e35e1c37e1
Sreyajit <sreyajit25@gmail.com>
2022-12-23 15:10:34 +0530

    Introducing The Django admin

--- FirstDjangoApp/polls/models.py
+++ FirstDjangoApp/polls/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField(auto_now = True)
+    pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
