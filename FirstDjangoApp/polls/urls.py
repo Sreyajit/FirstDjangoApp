be6cdc5106105ada664f911e6f8b93fb2e588fc1
Sreyajit <sreyajit25@gmail.com>
2022-12-28 14:11:09 +0530

    Tutorial part-3 starts, Views tested

--- FirstDjangoApp/polls/urls.py
+++ FirstDjangoApp/polls/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import Response 
+from .views import Detail, Homepage, Results, Vote
 urlpatterns = [
-        path('', Response.as_view(), name='index'),
+        path('', Homepage.as_view(), name='index'),
+        path('<int:question_id>/', Detail.as_view(), name='detail'),
+        path('<int:question_id>/results/', Results.as_view(), name='results'),
+        path('<int:question_id>/vote/', Vote.as_view(), name='vote'),
 ]
--- FirstDjangoApp/polls/views.py
+++ FirstDjangoApp/polls/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views import View
-class Response(View):
-    def get(self, request):
-        return HttpResponse("hello world")
+from django.views.generic import View, DetailView
+class Homepage(View):
+    def get(self, request, question_id):
+        return HttpResponse("You are looking at question %s." % question_id)
+class Detail(DetailView):
+    def get(self, request, question_id):
+        return HttpResponse("You are looking at question %s." % question_id)
+class Results(View):
+    def get(self, request, question_id):
+        response = "You're looking at the results of question %s."
+        return HttpResponse(response % question_id)
+class Vote(View):
+    def get(self, request, question_id):
+        return HttpResponse("You're voting on question %s." % question_id)
 # Create your views here.
