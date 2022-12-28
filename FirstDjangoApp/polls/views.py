c93d315f3a09af6bb30dfdf8e8d6ed351b910b3c
Sreyajit <sreyajit25@gmail.com>
2022-12-28 15:21:39 +0530

    Giving actual functionality to these views

--- FirstDjangoApp/polls/views.py
+++ FirstDjangoApp/polls/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View, DetailView
+from .models import Question
 class Homepage(View):
-    def get(self, request, question_id):
-        return HttpResponse("You are looking at question %s." % question_id)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = " ,".join([q.question_text for q in latest_question_list])
+    def get(self, request):
+        return HttpResponse(self.output)
 class Detail(DetailView):
     def get(self, request, question_id):
         return HttpResponse("You are looking at question %s." % question_id)
